c3e92e0ba18220495c181caa435b3de1971d6381
Charlie Bellow <charliebftm@gmail.com>
2022-09-11 11:45:52 -0300

    voltando pra versão

--- repositoriolgbt-react/src/JS/Autor.js
+++
@@ -1,8 +0,0 @@
-export class Autor {
-	constructor(nome, sobrenome, formacao, publicacoes) {
-		this.nome = nome;
-		this.sobrenome = sobrenome;
-		this.formacao = formacao;
-		this.publicacoes = publicacoes;
-	}
-}
