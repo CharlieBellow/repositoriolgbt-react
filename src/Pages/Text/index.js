66229501e69596f8005907b72905f61f3eb774af
Charlie Bellow <charliebftm@gmail.com>
2022-09-18 17:49:56 -0300

    sdsçlmvsldkfmdDDDDD

--- src/Pages/Text/index.js
+++ src/Pages/Text/index.js
@@ -17,7 +17,8 @@ function Text() {
 			 - colocar o projeto na raiz onde tá o git e fazer o commit disso e depois testar
 			 - subir o projeto pela master na pasta/docs e colocar o build dentro da pasta /docs - nao rolou
 			 - criar um repositório separado para fazer o deploy - também não rolou! desisti!
-			 -ultimo teste
+			 -ultimo teste - não rolou
+			 
 			 * ajeitar o css do filtro e do site todo (ver a questão do icone)
 			 */}
 			<Footer />
