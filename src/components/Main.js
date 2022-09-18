68768296c81b0aca2fce6da4ed70660ae79d8ef3
Charlie Bellow <charliebftm@gmail.com>
2022-09-18 17:09:07 -0300

    corrigindo o main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import Text from "../Pages/Text";
+//import Text from "../Pages/Text";
 
 
 import { useState } from "react";
@@ -186,7 +186,7 @@ function onCheckboxClick(checkboxArray) {
 
 }
 
-	const url = Text;
+	//const url = Text;
 
 
 	return (
