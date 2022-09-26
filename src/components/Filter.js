0e263f5b727668c7e3a66017184ed3408b7b6607
Charlie Bellow <charliebftm@gmail.com>
2022-09-26 11:43:54 -0300

    filtro finalizado e funcionando

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -35,55 +35,45 @@ function Filter({ onCheckboxClick }) {
 		onCheckboxClick(updatedCheckboxes);
 	}
 
-	const [className, setClassName] = useState('');
-
-	function handleClass(event) {
-		
-		if (event.target.className === "") {
-			event.target.classList.toggle("active");
-		} else {
-			event.target.classList.toggle("")
-		}
-	} 
-			
-
-//		if (className !== useState) {
-//			setClassName(...className, !"active")
-//		} else {
-//			setClassName(...className)
-//
-		//}
-	//}
-
 
 	return (
 		<>
 			<div calssName="content flex">
-				<div className={`filter__ret--purple content ${className}`}>
-					<div className={`${className}`}>
-						<FaChevronRight
-							className="icon__svg"
-							size={16}
-							onClick={handleClass}
-						/>
-					</div>
-					<div name="filtro" className={`filter__form ${className}`}>
-						<h3 className="h3">Filtro:</h3>
-						<div>
-							{listChecks.map(checkbox => (
-								<div key={checkbox.name}>
-									<input
-										className="checkbox"
-										type="checkbox"
-										name={checkbox.name}
-										id={checkbox.name}
-										onClick={handleClick}
-										checked={checkbox.check}
-									/>
-									<label htmlFor={checkbox.name}>{checkbox.name}</label>
-								</div>
-							))}
+				<div className={`filter__ret--purple content`}>
+					<input
+						type="checkbox"
+						name="toggle"
+						id="toggle"
+						className={`input-checkbox`}
+					/>
+
+					<div className="checkbox-wrapper">
+						<div name="filtro" className={`filter__form `}>
+							<h3 className="h3">Filtro:</h3>
+							<div>
+								{listChecks.map(checkbox => (
+									<div key={checkbox.name}>
+										<input
+											className="checkbox"
+											type="checkbox"
+											name={checkbox.name}
+											id={checkbox.name}
+											onClick={handleClick}
+											checked={checkbox.check}
+										/>
+										<label htmlFor={checkbox.name}>{checkbox.name}</label>
+									</div>
+								))}
+							</div>
 						</div>
+
+						<label htmlFor="toggle">
+							<FaChevronRight
+								className="icon__svg"
+								size={24}
+								//onClick={handleClass}
+							/>
+						</label>
 					</div>
 				</div>
 			</div>
